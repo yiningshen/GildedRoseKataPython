14f6cb039b485c64a62a2436ed0d2a93aa7e62b1
yiningshen <yiningshen0423@gmail.com>
2025-02-12 23:03:52 -0800

    upload lab 5

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from abc import ABC, abstractmethod
 
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
@@ -18,32 +18,41 @@ class GildedRose(object):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+class ItemStrategy(ABC):
+    @abstractmethod
+    def update_quality(self, item: Item) -> None:
+        pass
+
+class DefaultItemStrategy(ItemStrategy):
+    def update_quality(self, item: Item) -> None:
+        if item.quality > 0:
+            item.quality = item.quality - 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality = item.quality - 1
+
+class AgedBrieStrategy(ItemStrategy):
+    def update_quality(self, item: Item) -> None:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality = item.quality + 1
+
+class BackstagePassStrategy(ItemStrategy):
+    def update_quality(self, item: Item) -> None:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.sell_in < 11 and item.quality < 50:
+                item.quality = item.quality + 1
+            if item.sell_in < 6 and item.quality < 50:
+                item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = 0
+
+class SulfurasStrategy(ItemStrategy):
+    def update_quality(self, item: Item) -> None:
+        # Sulfuras never decreases in quality and doesn't need to be sold
+        pass
+
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,62 +1,48 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+import unittest
+from gilded_rose import Item, GildedRose, DefaultItemStrategy, AgedBrieStrategy, BackstagePassStrategy, SulfurasStrategy
+
 
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
         items = [Item("foo", 0, 0)]
 
-    # example of test that checks for logical errors
-    def test_sulfuras_should_not_decrease_quality(self):
-        items = [Item("Sulfuras", 5, 80)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
-        sulfuras_item = items[0]
-        self.assertEquals(80, sulfuras_item.quality)
-        self.assertEquals(4, sulfuras_item.sell_in)
-        self.assertEquals("Sulfuras", sulfuras_item.name)
+    def test_default_item_quality_decrease(self):
+        item = Item("regular item", 5, 10)
+        strategy = DefaultItemStrategy()
+        strategy.update_quality(item)
+        self.assertEqual(9, item.quality)  
+        self.assertEqual(4, item.sell_in)  
 
-    # example of test that checks for syntax errors
-    def test_gilded_rose_list_all_items(self):
-        items = [Item("Sulfuras", 5, 80)]
-        gilded_rose = GildedRose(items)
-        all_items = gilded_rose.get_items()
-        self.assertEquals(["Sulfuras"], all_items)
+    def test_aged_brie_quality_increase(self):
+        item = Item("Aged Brie", 5, 10)
+        strategy = AgedBrieStrategy()
+        strategy.update_quality(item)
+        self.assertEqual(11, item.quality)  
+        self.assertEqual(4, item.sell_in)   
 
-    # logical test-1
-    def test_negative_item_quantity(self):
-        # Test fails beacuse items cannot have negative quantity
-        items = [Item("Aged Brie", 5,-10 )]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(0, items[0].quality)
+    def test_backstage_pass_quality_rules(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 8, 20)
+        strategy = BackstagePassStrategy()
+        strategy.update_quality(item)
+        self.assertEqual(22, item.quality)  
+        self.assertEqual(7, item.sell_in)   
 
-    # logical test-2
-    def test_aged_brie_quality_over_50(self):
-        # Test fails because Aged Brie can exceed quality 50 when multiple
-        # quality increases are applied
-        items = [Item("Aged Brie", -1, 49)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(50, items[0].quality)
-
-    # logical test-3
-    def test_quality_degradation_after_sell_by(self):
-        #Test fails because quality should degrade twice as fast after sell_in < 0
-        items = [Item("regular item", 0, 10)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(8, items[0].quality)
-
-    # syntax test-1
-    def test_item_with_1_argument(self):
-        # Test fails because quantity of argument
-        items = [Item("Sulfuras")]
-        gilded_rose = GildedRose(items)
-        self.assertEquals(["Sulfuras"], all_items)
+    # Syntax Test
+    def test_strategy_inheritance(self):
+        strategies = [
+            DefaultItemStrategy(),
+            AgedBrieStrategy(),
+            BackstagePassStrategy(),
+            SulfurasStrategy()
+        ]
+        
+        for strategy in strategies:
+            self.assertTrue(hasattr(strategy, 'update_quality'))
+            self.assertTrue(callable(getattr(strategy, 'update_quality')))
 
 if __name__ == '__main__':
     unittest.main()
